a3fda6bf01d0720da4cd633dc44a0c3e36f2100b
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-22 15:42:36 +0000

    LabCompleted

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import org.eclipse.jetty.util.LeakDetector;
+
 public class Lab {
     /**
      * Return a string with an array's contents, formatted with spaces.
@@ -30,6 +32,14 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str=" ";
+        for(int i = 0; i < nums.length; i++)
+        {
+            //nums[i]= i;
+            str = str + " " + nums[i];
+            
+        }
+        return str;
+        
     }
 }
